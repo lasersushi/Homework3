387df6800df472e1523af9cae1618be06d154384
lasersushi <lwalker30@priorypanther.com>
2026-09-06 19:04:53 -0700

    fix: Changing units to meters

--- src/Homework3.java
+++ src/Homework3.java
@@ -53,7 +53,7 @@ public class Homework3 {
         return Alliance;
     }
     public static String mirrorXOnly(double x, double y, float heading) {
-        double xMirrored = fieldLenghtInches - x;
+        double xMirrored = fieldLenghtMeters - x;
         float headingMirrored = 180 - heading;
         String ans = "Your new pose is " + xMirrored + "," + y + ". Your new heading is" + headingMirrored;
         return ans;
